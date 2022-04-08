0fb518ce492e9715b00bd987b862b75c67b9471c
Martin Widarmadi <71425731+MartinWidarmadi@users.noreply.github.com>
2022-04-08 11:28:16 +0700

    enemy behavior
    ketika mendekat pada player, enemy akan mengikuti dan attack. Ketika mendekat pada bonfire, enemy akan menyerangnya

--- Scene/Enemies/enemy.gd
+++ Scene/Enemies/enemy.gd
@@ -1,12 +1,16 @@
 extends KinematicBody2D
 
-export var speed: int = 200
+export var speed: int = 100
 var velocity: Vector2 = Vector2.ZERO
 var path: Array = []
+var velocity1: Vector2 = Vector2.ZERO
+var path1: Array = []
 var navigation: Navigation2D = null
+var navigation1: Navigation2D = null
 var bonfire = null
-# Testing purpose
-#var player = null
+var player = null
+var in_attack_area: bool = false
+var player_in_area:bool = false
 
 ##############
 #TAKE DAMAGE
@@ -17,44 +21,88 @@ var hp = 50
 var base_damage = 21
 
 onready var health_bar = $HealthBar
+onready var animation_player = $AnimationPlayer
 
 func _ready():
+	# check if the object have a group
 	var tree = get_tree()
 	if tree.has_group("Navigation"):
 		navigation = tree.get_nodes_in_group("Navigation")[0]
+		navigation1 = tree.get_nodes_in_group("Navigation")[0]
 	if tree.has_group("Bonfire"):
 		bonfire = tree.get_nodes_in_group("Bonfire")[0]
-#	if tree.has_group("Player"):
-#		player = tree.get_nodes_in_group("Player")[0]
+	if tree.has_group("Player"):
+		player = tree.get_nodes_in_group("Player")[0]
 	
 #	# HealthBar progression
 	health_bar.max_value = hp
 	health_bar.value = hp
 	health_bar.set_as_toplevel(true)
+	
+	# start the walk animation
+	animation_player.play("walk")
 		
 func _physics_process(delta):
-	if bonfire and navigation:
-		generate_path()
-		navigate()
-	move()
+	if bonfire and navigation:			
+		generate_bonfire_path()
+		navigate_the_bonfire()
+		if player_in_area:
+			generate_player_path()
+			navigate_the_player()
+			if in_attack_area:
+				animation_player.play("attack")
+			else:
+				animation_player.play("walk")
+			move_to_player()
+		move_to_bonfire()
 	
 #	# Set HealthBar Position
 	health_bar.set_position(position - Vector2(15,17))
-	
-func navigate():
+
+## Move to bonfire
+func navigate_the_bonfire():
 	if path.size() > 0:
 		velocity = global_position.direction_to(path[1]) * speed
 		
 		if global_position == path[0]:
 			path.pop_front()
-func generate_path():
+func generate_bonfire_path():
 	if navigation != null and bonfire != null:
 		path = navigation.get_simple_path(global_position, bonfire.global_position, true)
 
-		
-func move():
+func move_to_bonfire():
 	velocity = move_and_slide(velocity)
 
+## Move to player
+func navigate_the_player():
+	if path1.size() > 0:
+		velocity1 = global_position.direction_to(path1[1]) * speed
+		
+		if global_position == path1[0]:
+			path1.pop_front()
+			
+func generate_player_path():
+	if navigation1 != null and player != null:
+		path1 = navigation1.get_simple_path(global_position, player.global_position, true)
+
+func move_to_player():
+	velocity1 = move_and_slide(velocity1)
+	
+func _on_PlayerDetector_area_entered(area):
+	player_in_area = true
+	return player_in_area
+
+func _on_PlayerDetector_area_exited(area):
+	player_in_area = false
+	return player_in_area
+	
+func _on_AttackDetector_area_entered(area):
+	in_attack_area = true
+	return in_attack_area
+	
+func _on_AttackDetector_area_exited(area):
+	in_attack_area = false
+	return in_attack_area
 
 
 #onready var impact_area = $Impact
@@ -69,6 +117,19 @@ func on_hit(damage):
 		on_destroy()
 		
 func on_destroy():
-	$AnimatedSprite.queue_free()
+	animation_player.queue_free()
 	yield(get_tree().create_timer(0.2), "timeout")
 	self.queue_free()
+	
+
+
+
+
+
+
+
+
+
+
+
+

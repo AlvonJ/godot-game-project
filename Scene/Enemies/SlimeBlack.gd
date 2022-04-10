77f3fc7583fd40e7f0bfcaebdaf2ec32b998fcb6
Alvon Jovanus <71739326+AlvonJ@users.noreply.github.com>
2022-04-10 21:53:58 +0700

    menghilangkan return dari enemy script

--- Scene/Enemies/SlimeBlack.gd
+++ Scene/Enemies/SlimeBlack.gd
@@ -3,19 +3,15 @@ extends "enemy.gd"
 	
 func _on_PlayerDetector_area_entered(area):
 	player_in_area = true
-	return player_in_area
 
 func _on_PlayerDetector_area_exited(area):
 	player_in_area = false
-	return player_in_area
 	
 func _on_AttackDetector_area_entered(area):
 	in_attack_area = true
-	return in_attack_area
 	
 func _on_AttackDetector_area_exited(area):
 	in_attack_area = false
-	return in_attack_area
 
 
 

dc5f3f8e119c841837023a704bbcbb9bceaa75ca
Martin Widarmadi <71425731+MartinWidarmadi@users.noreply.github.com>
2022-06-01 10:48:20 +0700

    Update player
    Penambahan script untuk menghubungkan antara joystick dengan movement player

--- Scene/Player/Player.gd
+++ Scene/Player/Player.gd
@@ -10,58 +10,34 @@ var attack = false
 func _ready():
 	for i in get_tree().get_nodes_in_group("attack_button"):
 		i.connect("pressed", self, "initiate_attack")
-
+	for i in get_tree().get_nodes_in_group("joystick_button"):
+		i.connect("when_joystick_touched", self, "move_character")
 		
 func _physics_process(delta):
-	move_horizontal()
-	move_vertical()
+#	move_horizontal()
+#	move_vertical()
 	if attack:
 		$AnimationPlayer.play("attack")
 	else:
 		animate()
 	move_and_slide(motion)
-
+	print(motion)
 
 func move_horizontal():
-	if (global_position.x > 30 and global_position.x < 1250):
-		if Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
-			motion.x = SPEED
-		elif Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
-			motion.x = -SPEED
-		else:
-			motion.x = 0
-	elif (global_position.x <= 30):
-		if Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
-			motion.x = SPEED
-		else:
-			motion.x = 0
-	elif (global_position.x >= 1250):
-		if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
-			motion.x = -SPEED
-		else:
-			motion.x = 0
-		
-		
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else:
+		motion.x = 0
 		
 func move_vertical():
-	if (global_position.y > 30 and global_position.y < 690):	
-		if Input.is_action_pressed("up"):
-			motion.y = -SPEED
-		elif Input.is_action_pressed("down"):
-			motion.y = SPEED
-		else:
-			motion.y = 0
-	elif global_position.y <= 30:
-		if Input.is_action_pressed("down"):
-			motion.y = SPEED
-		else:
-			motion.y = 0
-	elif global_position.y >= 690:
-		if Input.is_action_pressed("up"):
-			motion.y = -SPEED
-		else:
-			motion.y = 0
-		
+	if Input.is_action_pressed("up"):
+		motion.y = -SPEED
+	elif Input.is_action_pressed("down"):
+		motion.y = SPEED
+	else:
+		motion.y = 0
 	
 func animate():
 	if motion.x == 0 and motion.y == 0:
@@ -85,4 +61,7 @@ func stop_attack():
 func _on_HitBox_body_entered(body):
 	if body.is_in_group("Enemy"):
 		body.on_hit(GameData.human_builder["damage"])
-		
+
+func _on_Joystick_touched(velocity):
+	# Move horizontal
+	motion = (velocity * SPEED)

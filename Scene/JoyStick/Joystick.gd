723ae2d05a029f1e50ccae8db044148e5ae597cb
Martin Widarmadi <71425731+MartinWidarmadi@users.noreply.github.com>
2022-06-01 12:43:57 +0700

    Update Joystick
    Scene GameScene hanya ada UI, tidak ada lagi Templatelevel sehingga memerlukan beberapa penyesuaian

--- Scene/JoyStick/Joystick.gd
+++ Scene/JoyStick/Joystick.gd
@@ -9,9 +9,6 @@ var touched = false
 
 var velocity = Vector2(0, 0)
 
-signal touched(velocity)
-
-
 func _input(event):
 	if event is InputEventScreenTouch:
 		var distance = event.position.distance_to(big_circle.global_position)
@@ -26,11 +23,9 @@ func _process(delta):
 	if touched:
 		 small_circle.global_position = get_global_mouse_position()
 		 small_circle.position = big_circle.position + (small_circle.position - big_circle.position).clamped(max_distance)
-		 velocity.x = small_circle.position.x / max_distance
-		 velocity.y = small_circle.position.y / max_distance
-	else:
-		velocity.x = 0
-		velocity.y = 0
-	print(velocity)
-	emit_signal("touched", velocity.normalized())
+
+func player_velocity():
+	velocity.x = small_circle.position.x / max_distance
+	velocity.y = small_circle.position.y / max_distance
+	return velocity.normalized()
 	

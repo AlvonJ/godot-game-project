e8d9419e8786e2dde88b813737952c1ef64f1f00
Alvon Jovanus <71739326+AlvonJ@users.noreply.github.com>
2022-06-01 20:29:31 +0700

    update code on gamescene and buttons

--- Scene/Levels/Select/Button.gd
+++ Scene/Levels/Select/Button.gd
@@ -8,7 +8,6 @@ func _on_Button_pressed():
 	get_node('/root/SceneHandler/HUD').queue_free()
 	var game_scene = load("res://Scene/MainScenes/GameScene.tscn").instance()
 	game_scene.connect("game_finished", get_node('/root/SceneHandler'), "unload_game")
+	game_scene.map_node = load(level_path).instance()
 	get_node('/root/SceneHandler').add_child(game_scene)
 	
-	if not get_node('/root/SceneHandler/GameScene').has_node(level):
-		get_node('/root/SceneHandler/GameScene').add_child(load(level_path).instance())

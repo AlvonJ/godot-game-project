28064e16346efb449d65dca9ee6a28c713224f5b
Alvon Jovanus <71739326+AlvonJ@users.noreply.github.com>
2022-06-02 07:10:51 +0700

    edit evileye.gd

--- Scene/Enemies/EvilEye.gd
+++ Scene/Enemies/EvilEye.gd
@@ -1,1 +1,20 @@
 extends "enemy.gd"
+
+func _on_PlayerDetector_area_entered(area):
+	if area.is_in_group("detector"):
+		player_in_area = true
+
+func _on_PlayerDetector_area_exited(area):
+	if area.is_in_group("detector"):
+		player_in_area = false
+	
+func _on_AttackDetector_area_entered(area):
+	if area.is_in_group("detector"):
+		in_attack_area = true
+	
+func _on_AttackDetector_area_exited(area):
+	if area.is_in_group("detector"):
+		in_attack_area = false
+
+
+
